ed93ca5db37e00f8f184020b8b6278dbea78dc03
Marko Elezovic <marko@element.hr>
2015-08-28 18:23:41 +0200

    Added protocolVersion=2 to PostgresConnection.execute

--- src/test/java/com/dslplatform/compiler/client/parameters/PostgresConnectionTest.java
+++ src/test/java/com/dslplatform/compiler/client/parameters/PostgresConnectionTest.java
@@ -2,6 +2,9 @@ package com.dslplatform.compiler.client.parameters;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.dslplatform.compiler.client.parameters.PostgresConnection.extractPostgresVersion;
 import static org.junit.Assert.assertEquals;
 
@@ -12,5 +15,61 @@ public class PostgresConnectionTest {
 		assertEquals("9.3", extractPostgresVersion("PostgreSQL 9.3.0 on x86_64-unknown-linux-gnu, compiled by gcc (Debian 4.4.5-8) 4.4.5, 64-bit", null));
 		assertEquals("9.3", extractPostgresVersion("PostgreSQL 9.3.1 on x86_64-unknown-linux-gnu, compiled by gcc (Debian 4.7.2-5) 4.7.2, 64-bit", null));
 		assertEquals("9.4", extractPostgresVersion("PostgreSQL 9.4beta2, compiled by Visual C++ build 1800, 64-bit", null));
+	}
+
+	@Test
+	public void testSplitConnectionString() {
+		assertEquals(
+				PostgresConnection.splitConnectionString("localhost/Database"),
+				new HashMap<String, String>() {{
+					put(null, "localhost/Database");
+				}});
+
+		assertEquals(
+				PostgresConnection.splitConnectionString("my.server.com:5432/MyDatabase?protocolVersion=1&user=user&password=password"),
+				new HashMap<String, String>() {{
+					put(null, "my.server.com:5432/MyDatabase");
+					put("protocolVersion", "1");
+					put("user", "user");
+					put("password", "password");
+				}});
+
+		assertEquals(
+				PostgresConnection.splitConnectionString("server:5432/DB?user=migration&password=food=good&ssl=true&protocolVersion=2"),
+				new HashMap<String, String>() {{
+					put(null, "server:5432/DB");
+					put("user", "migration");
+					put("password", "food=good");
+					put("ssl", "true");
+					put("protocolVersion", "2");
+				}});
+
+		assertEquals(
+				PostgresConnection.splitConnectionString("my.server.com:5432/MyDatabase?empty1=&empty2=&empty3"),
+				new HashMap<String, String>() {{
+					put(null, "my.server.com:5432/MyDatabase");
+					put("empty1", "");   // equals
+					put("empty2", "");   // equals
+					put("empty3", null); // no equals
+				}});
+	}
+
+	@Test
+	public void testJoinConnectionParams() {
+		final Map<String, String> p1 = PostgresConnection.splitConnectionString("localhost/Database");
+		p1.put("protocolVersion", "2");
+		assertEquals("localhost/Database?protocolVersion=2", PostgresConnection.joinConnectionParams(p1));
+
+		final Map<String, String> p2 = PostgresConnection.splitConnectionString("my.server.com:5432/MyDatabase?protocolVersion=1&user=user&password=password");
+		p2.put("protocolVersion", "2");
+		assertEquals("my.server.com:5432/MyDatabase?protocolVersion=2&user=user&password=password", PostgresConnection.joinConnectionParams(p2));
+
+		final Map<String, String> p3 = PostgresConnection.splitConnectionString("server:5432/DB?user=migration&password=food=good&ssl=true&protocolVersion=2");
+		p3.put("protocolVersion", "2");
+		assertEquals("server:5432/DB?user=migration&password=food=good&ssl=true&protocolVersion=2", PostgresConnection.joinConnectionParams(p3));
+
+		final Map<String, String> p4 = PostgresConnection.splitConnectionString("my.server.com:5432/MyDatabase?empty1=&empty2=&empty3");
+		p4.put("protocolVersion", "2");
+		assertEquals("my.server.com:5432/MyDatabase?empty1=&empty2=&empty3&protocolVersion=2", PostgresConnection.joinConnectionParams(p4));
 	}
 }
