80eff16a4ced9f885c9dedb6d70ef8968015b50e
Rikard Pavelic <rikard@ngs.hr>
2016-10-15 22:20:22 +0200

    New IDEA plugin
    Cache highlighter per project/file pair.
    This resolves the issue of disposed project used in highlighter.

--- IdeaPlugin/src/com/dslplatform/ideaplugin/DslSyntaxHighlighterFactory.java
+++ IdeaPlugin/src/com/dslplatform/ideaplugin/DslSyntaxHighlighterFactory.java
@@ -10,14 +10,35 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class DslSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
-	private final ConcurrentMap<VirtualFile, SyntaxHighlighter> highlighters = new ConcurrentHashMap<VirtualFile, SyntaxHighlighter>();
+	private static class MapKey {
+		private final Project project;
+		private final VirtualFile file;
+		MapKey(Project project, VirtualFile file) {
+			this.project = project;
+			this.file = file;
+		}
+
+		@Override
+		public int hashCode() {
+			return project.hashCode() ^ file.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof MapKey)) return false;
+			MapKey mk = (MapKey)obj;
+			return mk.project.equals(this.project) && mk.file.equals(this.file);
+		}
+	}
+	private final ConcurrentMap<MapKey, DslSyntaxHighlighter> highlighters = new ConcurrentHashMap<MapKey, DslSyntaxHighlighter>();
 	@NotNull
 	@Override
 	public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
-		SyntaxHighlighter highlighter = highlighters.get(virtualFile);
+		MapKey key = new MapKey(project, virtualFile);
+		DslSyntaxHighlighter highlighter = highlighters.get(key);
 		if (highlighter == null) {
 			highlighter = new DslSyntaxHighlighter(project, virtualFile);
-			highlighters.put(virtualFile, highlighter);
+			highlighters.put(key, highlighter);
 		}
 		return highlighter;
 	}
